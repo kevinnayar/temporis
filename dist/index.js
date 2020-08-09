5c898710d4587c25c68482f1bb40acedf998bd03
kevinnayar <kevin.nayar@gmail.com>
2020-08-09 11:49:48 -0500

    ts configs

--- dist/index.js
+++ dist/index.js
@@ -1,70 +1,58 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.useTemporis = void 0;
-var use_temporis_1 = require("./use-temporis");
-var createSeriesItem = function (item) { return ({ item: item, isCurrent: true }); };
-var findCurrentItemIndex = function (history) { return history.findIndex(function (h) { return h.isCurrent === true; }); };
+const use_temporis_1 = require("./use-temporis");
+const createSeriesItem = (item) => ({ item, isCurrent: true });
+const findCurrentItemIndex = (history) => history.findIndex((h) => h.isCurrent === true);
 exports.useTemporis = use_temporis_1.useTemporis;
 function Temporis(limit) {
-    var _history = [];
-    var _limit = limit || 100;
-    var pushOne = function (item) {
-        var index = findCurrentItemIndex(_history);
-        var currentItem = _history[index];
+    const _history = [];
+    const _limit = limit || 100;
+    const pushOne = (item) => {
+        const index = findCurrentItemIndex(_history);
+        const currentItem = _history[index];
         if (currentItem !== undefined) {
             _history[index].isCurrent = false;
         }
-        var seriesItem = createSeriesItem(item);
+        const seriesItem = createSeriesItem(item);
         _history.push(seriesItem);
         if (_history.length > _limit) {
             _history.shift();
         }
     };
-    var pushMany = function (items) {
-        for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
-            var item = items_1[_i];
+    const pushMany = (items) => {
+        for (const item of items) {
             pushOne(item);
         }
     };
-    var undo = function () {
-        var index = findCurrentItemIndex(_history);
-        var currentItem = _history[index];
-        var prevItem = _history[index - 1];
+    const undo = () => {
+        const index = findCurrentItemIndex(_history);
+        const currentItem = _history[index];
+        const prevItem = _history[index - 1];
         if (currentItem !== undefined && prevItem !== undefined) {
             _history[index].isCurrent = false;
             _history[index - 1].isCurrent = true;
         }
     };
-    var redo = function () {
-        var index = findCurrentItemIndex(_history);
-        var currentItem = _history[index];
-        var nextItem = _history[index + 1];
+    const redo = () => {
+        const index = findCurrentItemIndex(_history);
+        const currentItem = _history[index];
+        const nextItem = _history[index + 1];
         if (currentItem !== undefined && nextItem !== undefined) {
-            _history[index] = __assign(__assign({}, currentItem), { isCurrent: false });
-            _history[index + 1] = __assign(__assign({}, nextItem), { isCurrent: true });
+            _history[index] = { ...currentItem, isCurrent: false };
+            _history[index + 1] = { ...nextItem, isCurrent: true };
         }
     };
-    var getCurrentItem = function () {
-        var currentItemMaybe = _history.find(function (h) { return h.isCurrent === true; });
+    const getCurrentItem = () => {
+        const currentItemMaybe = _history.find((h) => h.isCurrent === true);
         return currentItemMaybe ? currentItemMaybe.item : undefined;
     };
     return {
-        pushOne: pushOne,
-        pushMany: pushMany,
-        undo: undo,
-        redo: redo,
-        getCurrentItem: getCurrentItem
+        pushOne,
+        pushMany,
+        undo,
+        redo,
+        getCurrentItem,
     };
 }
-exports["default"] = Temporis;
+exports.default = Temporis;
--- dist/use-temporis.js
+++ dist/use-temporis.js
@@ -1,30 +1,30 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.useTemporis = void 0;
-var react_1 = require("react");
+const react_1 = require("react");
 function useTemporis(temporisInstance, initialState) {
-    var _a = react_1.useState(initialState), items = _a[0], setItems = _a[1];
+    const [items, setItems] = react_1.useState(initialState);
     function pushOne(items) {
         temporisInstance.pushOne(items);
         setItems(items);
     }
     function undo() {
         temporisInstance.undo();
-        var current = temporisInstance.getCurrentItem();
+        const current = temporisInstance.getCurrentItem();
         if (current)
             setItems(current);
     }
     function redo() {
         temporisInstance.redo();
-        var current = temporisInstance.getCurrentItem();
+        const current = temporisInstance.getCurrentItem();
         if (current)
             setItems(current);
     }
     return {
-        items: items,
-        pushOne: pushOne,
-        undo: undo,
-        redo: redo
+        items,
+        pushOne,
+        undo,
+        redo,
     };
 }
 exports.useTemporis = useTemporis;
